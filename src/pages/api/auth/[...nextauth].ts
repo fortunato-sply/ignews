f8a70a4e46f72ed2ed5d3bfb92eae4cf90b71211
fortunato-sply <lucasfortunato2530@gmail.com>
2022-07-26 03:48:28 -0300

    added connection with FaunaDB

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,5 +1,11 @@
+import { query as q } from 'faunadb'
+
 import NextAuth from 'next-auth';
 import GithubProvider from 'next-auth/providers/github';
+
+import { fauna } from '../../../services/fauna'
+
+import signIn from 'next-auth'
 
 export default NextAuth({
   providers: [
@@ -8,4 +14,33 @@ export default NextAuth({
       clientSecret: process.env.GITHUB_CLIENT_SECRET,
     }),
   ],
+  callbacks: {
+    async signIn( User ) {
+      const email = User.user.email
+
+      const searchUserByEmail = q.Match(
+        q.Index('user_by_email'),
+        q.Casefold(email)
+      )
+
+      try {
+        await fauna.query(
+          q.If(
+            q.Not(
+              q.Exists(searchUserByEmail)
+            ),
+            q.Create(
+              q.Collection('users'),
+              { data: { email } }
+            ),
+            q.Get(searchUserByEmail)
+          )
+        )
+
+        return true
+      } catch {
+        return
+      }
+    }
+  },
 })
